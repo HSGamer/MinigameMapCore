83042d91771ed27e208f198cef6a2f9b004b2163
HSGamer <huynhqtienvtag@gmail.com>
2022-02-19 22:57:28 +0700

    SkyWarsOptions

---
+++ skywars-api/src/main/java/me/hsgamer/minigamemapcore/skywars/api/SkyWarsOptions.java
@@ -0,0 +1,13 @@
+package me.hsgamer.minigamemapcore.skywars.api;
+
+@SuppressWarnings("unused")
+public final class SkyWarsOptions {
+    private SkyWarsOptions() {
+        // EMPTY
+    }
+
+    private static final String TEAM_SIZE = "team-size";
+    private static final String MIN_TEAMS = "min-teams";
+    private static final String MAX_TEAMS = "max-teams";
+    private static final String DISPLAY_NAME = "display-name";
+}
