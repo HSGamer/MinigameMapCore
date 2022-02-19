d4928201222128c714ebb4658e7218b1f4e59f84
HSGamer <huynhqtienvtag@gmail.com>
2022-02-19 23:10:50 +0700

    point

--- skywars-api/src/main/java/me/hsgamer/minigamemapcore/skywars/api/SkyWarsOptions.java
+++ skywars-api/src/main/java/me/hsgamer/minigamemapcore/skywars/api/SkyWarsOptions.java
@@ -10,4 +10,6 @@ public final class SkyWarsOptions {
     public static final String MIN_TEAMS = "min-teams";
     public static final String MAX_TEAMS = "max-teams";
     public static final String DISPLAY_NAME = "display-name";
+    public static final String MIN_POINT = "min-point";
+    public static final String MAX_POINT = "max-point";
 }
