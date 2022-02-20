16285c0062733d8cacd112cd4a461421bf09875f
HSGamer <huynhqtienvtag@gmail.com>
2022-02-20 18:23:47 +0700

    internal name

--- skywars-api/src/main/java/me/hsgamer/minigamemapcore/skywars/api/SkyWarsConstants.java
+++ skywars-api/src/main/java/me/hsgamer/minigamemapcore/skywars/api/SkyWarsConstants.java
@@ -10,6 +10,7 @@ public final class SkyWarsConstants {
         public static final String TEAM_SIZE = "team-size";
         public static final String MIN_TEAMS = "min-teams";
         public static final String MAX_TEAMS = "max-teams";
+        public static final String INTERNAL_NAME = "internal-name";
         public static final String DISPLAY_NAME = "display-name";
         public static final String MIN_POINT = "min-point";
         public static final String MAX_POINT = "max-point";
