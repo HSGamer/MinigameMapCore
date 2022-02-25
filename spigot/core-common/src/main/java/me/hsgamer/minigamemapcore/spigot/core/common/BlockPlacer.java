14f928e8891a91d725cc30810552ef7949415671
HSGamer <huynhqtienvtag@gmail.com>
2022-02-25 22:35:02 +0700

    would not check isSolid

--- spigot/core-common/src/main/java/me/hsgamer/minigamemapcore/spigot/core/common/BlockPlacer.java
+++ spigot/core-common/src/main/java/me/hsgamer/minigamemapcore/spigot/core/common/BlockPlacer.java
@@ -1,12 +1,9 @@
 package me.hsgamer.minigamemapcore.spigot.core.common;
 
-import me.hsgamer.hscore.common.Pair;
 import me.hsgamer.minigamemapcore.api.data.BlockFormatData;
 import me.hsgamer.minigamemapcore.api.data.BlockPosition;
 import org.bukkit.World;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Map;
 
 public interface BlockPlacer {
@@ -15,14 +12,6 @@ public interface BlockPlacer {
     void place(World world, BlockPosition position, BlockFormatData data);
 
     default void place(World world, Map<BlockPosition, BlockFormatData> blockFormatDataMap) {
-        List<Pair<BlockPosition, BlockFormatData>> nonSolidList = new ArrayList<>();
-        blockFormatDataMap.forEach((position, formatData) -> {
-            if (isSolid(formatData.getMaterial())) {
-                place(world, position, formatData);
-            } else {
-                nonSolidList.add(Pair.of(position, formatData));
-            }
-        });
-        nonSolidList.forEach(pair -> place(world, pair.getKey(), pair.getValue()));
+        blockFormatDataMap.forEach((position, formatData) -> place(world, position, formatData));
     }
 }
